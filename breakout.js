f5ef2974152edd1b7dffd00dad03f31a02103cf7
Alan Luu <alanluu4@gmail.com>
2018-03-28 00:12:54 +0000

    Second commit

---
+++ breakout.js
@@ -0,0 +1,173 @@
+/**
+ * This is a simple breakout game made using JavaScript WITHOUT using ANY external frameworks, such as Phaser.js.
+*/ 
+
+/* global $ */
+
+"use strict";
+
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+var ballRadius = 10;
+var x = canvas.width / 2;
+var y = canvas.height - 30;
+var dx = 2;
+var dy = -2;
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width - paddleWidth) / 2;
+var interval;
+var brickRowCount = 3; //A row has 3 bricks.
+var brickColumnCount = 5; //A column has 5 bricks.
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+var score = 0;
+var lives = 1;
+
+//A 2D array of the bricks.
+var bricks = [];
+function renderBricks() {
+    for (var c = 0; c < brickColumnCount; c++) {
+        bricks[c] = [];
+        for (var r = 0; r < brickRowCount; r++) {
+            bricks[c][r] = { 
+                x: 0, 
+                y: 0, 
+                state: 1
+            };
+        }
+    }
+}
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawBricks() {
+    for (var c = 0; c < brickColumnCount; c++) {
+        for (var r = 0; r < brickRowCount; r++) {
+            if (bricks[c][r].state == 1) {
+                var brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
+                var brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = "#0095DD";
+                ctx.fill();
+                ctx.closePath();
+            }
+        }
+    }
+}
+
+//This function gets called first!
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBricks();
+    drawBall();
+    drawPaddle();
+    drawScore();
+    drawLives();
+    collisionDetection();
+    
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if (y + dy < ballRadius) {
+        dy = -dy;
+    } else if (y + dy > canvas.height - ballRadius) {
+        if (x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+        } else {
+            lives--;
+            if (lives <= 0) {
+                ctx.font = "30px Arial";
+                ctx.fillText("GAME OVER", 160, 180);
+                clearInterval(interval);
+                interval = null;
+            } else {
+                x = canvas.width / 2;
+                y = canvas.height - 30;
+                dx = 2;
+                dy = -2;
+                paddleX = (canvas.width - paddleWidth) / 2;
+            }
+        }
+    }
+    
+    x += dx;
+    y += dy;
+}
+
+function collisionDetection() {
+    for (var c = 0; c < brickColumnCount; c++) {
+        for (var r = 0; r < brickRowCount; r++) {
+            var b = bricks[c][r];
+            if (b.state == 1) {
+                if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+                    dy = -dy;
+                    b.state = 0;
+                    score++;
+                    if (score == brickRowCount * brickColumnCount) {
+                        ctx.font = "30px Arial";
+                        ctx.fillText("YOU WIN", 160, 180);
+                        clearInterval(interval);
+                    }
+                }
+            }
+        }
+    }
+}
+
+function drawScore() {
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#0095DD";
+    ctx.fillText("Score: "+score, 8, 20);
+}
+
+function drawLives() {
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#0095DD";
+    ctx.fillText("Lives: " + lives, canvas.width - 65, 20);
+}
+
+function mouseMoved(mouse) {
+    var relativeX = mouse.clientX - canvas.offsetLeft;
+    if (relativeX > 0 && relativeX < canvas.width) {
+        paddleX = relativeX - paddleWidth/2;
+    }
+}
+
+$(document).ready(function() {
+    $("#canvasDim").html("The canvas width is " + canvas.width + " px, and the canvas height is " + canvas.height + " px.");
+    $("#startButton").click(function() {
+        renderBricks();
+        if (typeof interval == "undefined" || interval === null) {
+            if (lives == 0) lives = 1, x = canvas.width / 2, y = canvas.height - 30, dx = 2, dy = -2;
+            interval = setInterval(draw, 15);
+            document.addEventListener("mousemove", mouseMoved, false);
+        }
+    });
+});
+
+(function() {
+    renderBricks();
+    drawBricks();
+})();
