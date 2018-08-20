51b606c4fc30276c7431b4ca6c897573a20611ab
Alan Luu <alanluu4@gmail.com>
2018-08-20 01:22:46 +0000

    Update

---
+++ responsive.js
@@ -0,0 +1,10 @@
+(function() {
+    document.getElementById("navicon").addEventListener("click", function() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+            x.className += " responsive";
+        } else {
+            x.className = "topnav";
+        }
+    });
+})();
